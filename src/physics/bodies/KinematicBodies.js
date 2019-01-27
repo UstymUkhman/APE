9a761febe32552d92382afde6ea42f37099ca126
UstymUkhman <ustym.ukhman@gmail.com>
2019-01-27 17:17:46 +0100

    Kinematic & Static bodies documentation added

--- src/physics/bodies/KinematicBodies.js
+++ src/physics/bodies/KinematicBodies.js
@@ -1,3 +1,5 @@
+// Kinematic bodies class manager
+
 import RigidBody from 'physics/bodies/RigidBody';
 import { Ammo } from 'core/Ammo';
 
@@ -8,6 +10,12 @@ import {
 } from 'physics/constants';
 
 export default class KinematicBodies extends RigidBody {
+  /**
+   * @extends RigidBody
+   * @constructs KinematicBodies
+   * @description - Initialize kinematic bodies physics
+   * @param {Object} physicWorld - Ammo.js soft/rigid or discrete dynamics world
+   */
   constructor (physicWorld) {
     super();
 
@@ -19,37 +27,68 @@ export default class KinematicBodies extends RigidBody {
     /* eslint-enable new-cap */
   }
 
+  /**
+   * @public
+   * @description - Add box-like collider to THREE.js mesh
+   * @param {Object} mesh - THREE.js mesh
+   */
   addBox (mesh) {
     const size = mesh.geometry.parameters;
     const box = super.createBox(size);
-    this.addKinematicBody(box, mesh);
+    this._addKinematicBody(box, mesh);
   }
 
+  /**
+   * @public
+   * @description - Add cylinder-like collider to THREE.js mesh
+   * @param {Object} mesh - THREE.js mesh
+   */
   addCylinder (mesh) {
     const size = mesh.geometry.parameters;
     const cylinder = super.createCylinder(size);
-    this.addKinematicBody(cylinder, mesh);
+    this._addKinematicBody(cylinder, mesh);
   }
 
+  /**
+   * @public
+   * @description - Add capsule-like collider to THREE.js mesh
+   * @param {Object} mesh - THREE.js mesh
+   */
   addCapsule (mesh) {
     const size = mesh.geometry.parameters;
     const capsule = super.createCapsule(size);
-    this.addKinematicBody(capsule, mesh);
+    this._addKinematicBody(capsule, mesh);
   }
 
+  /**
+   * @public
+   * @description - Add cone-like collider to THREE.js mesh
+   * @param {Object} mesh - THREE.js mesh
+   */
   addCone (mesh) {
     const size = mesh.geometry.parameters;
     const cone = super.createCone(size);
-    this.addKinematicBody(cone, mesh);
+    this._addKinematicBody(cone, mesh);
   }
 
+  /**
+   * @public
+   * @description - Add sphere-like collider to THREE.js mesh
+   * @param {Object} mesh - THREE.js mesh
+   */
   addSphere (mesh) {
     const size = mesh.geometry.parameters;
     const sphere = super.createSphere(size);
-    this.addKinematicBody(sphere, mesh);
+    this._addKinematicBody(sphere, mesh);
   }
 
-  addKinematicBody (shape, mesh) {
+  /**
+   * @private
+   * @description - Create kinematic body and add it to physics world
+   * @param {Object} shape - Ammo.js shape collider
+   * @param {Object} mesh - THREE.js mesh
+   */
+  _addKinematicBody (shape, mesh) {
     const position = mesh.position;
     const quaternion = mesh.quaternion;
     const body = super.createRigidBody(shape, ZERO_MASS, position, quaternion);
@@ -62,6 +101,11 @@ export default class KinematicBodies extends RigidBody {
     this.bodies.push(mesh);
   }
 
+  /**
+   * @public
+   * @description - Update kinematic bodies in requestAnimation loop
+   * @param {Object} transform - Ammo.js default btTransform
+   */
   update (transform) {
     for (let i = 0; i < this.bodies.length; i++) {
       const position = this.bodies[i].position;
--- src/physics/bodies/StaticBodies.js
+++ src/physics/bodies/StaticBodies.js
@@ -1,8 +1,16 @@
+// Static bodies class manager
+
 import { ZERO_MASS, DISABLE_DEACTIVATION } from 'physics/constants';
 import RigidBody from 'physics/bodies/RigidBody';
 import { Ammo } from 'core/Ammo';
 
 export default class StaticBodies extends RigidBody {
+  /**
+   * @extends RigidBody
+   * @constructs StaticBodies
+   * @description - Initialize static bodies physics
+   * @param {Object} physicWorld - Ammo.js soft/rigid or discrete dynamics world
+   */
   constructor (physicWorld) {
     super();
 
@@ -10,6 +18,12 @@ export default class StaticBodies extends RigidBody {
     this.world = physicWorld;
   }
 
+  /**
+   * @public
+   * @description - Add plane-like collider to THREE.js mesh
+   *                Used primarily to create ground/walls in physics world
+   * @param {Object} mesh - THREE.js mesh with PlaneBufferGeometry
+   */
   addPlane (mesh) {
     // Convert X-axis rotation from
     // THREE.js to Z-axis rotation in Ammo.js:
@@ -21,40 +35,71 @@ export default class StaticBodies extends RigidBody {
     /* eslint-enable new-cap */
 
     this.checkBodyMargin(plane);
-    this.addStaticBody(plane, mesh);
+    this._addStaticBody(plane, mesh);
   }
 
+  /**
+   * @public
+   * @description - Add box-like collider to THREE.js mesh
+   * @param {Object} mesh - THREE.js mesh
+   */
   addBox (mesh) {
     const size = mesh.geometry.parameters;
     const box = super.createBox(size);
-    this.addStaticBody(box, mesh);
+    this._addStaticBody(box, mesh);
   }
 
+  /**
+   * @public
+   * @description - Add cylinder-like collider to THREE.js mesh
+   * @param {Object} mesh - THREE.js mesh
+   */
   addCylinder (mesh) {
     const size = mesh.geometry.parameters;
     const cylinder = super.createCylinder(size);
-    this.addStaticBody(cylinder, mesh);
+    this._addStaticBody(cylinder, mesh);
   }
 
+  /**
+   * @public
+   * @description - Add capsule-like collider to THREE.js mesh
+   * @param {Object} mesh - THREE.js mesh
+   */
   addCapsule (mesh) {
     const size = mesh.geometry.parameters;
     const capsule = super.createCapsule(size);
-    this.addStaticBody(capsule, mesh);
+    this._addStaticBody(capsule, mesh);
   }
 
+  /**
+   * @public
+   * @description - Add cone-like collider to THREE.js mesh
+   * @param {Object} mesh - THREE.js mesh
+   */
   addCone (mesh) {
     const size = mesh.geometry.parameters;
     const cone = super.createCone(size);
-    this.addStaticBody(cone, mesh);
+    this._addStaticBody(cone, mesh);
   }
 
+  /**
+   * @public
+   * @description - Add sphere-like collider to THREE.js mesh
+   * @param {Object} mesh - THREE.js mesh
+   */
   addSphere (mesh) {
     const size = mesh.geometry.parameters;
     const sphere = super.createSphere(size);
-    this.addStaticBody(sphere, mesh);
+    this._addStaticBody(sphere, mesh);
   }
 
-  addStaticBody (shape, mesh) {
+  /**
+   * @private
+   * @description - Create static body and add it to physics world
+   * @param {Object} shape - Ammo.js shape collider
+   * @param {Object} mesh - THREE.js mesh
+   */
+  _addStaticBody (shape, mesh) {
     const position = mesh.position;
     const quaternion = mesh.quaternion;
     const body = super.createRigidBody(shape, ZERO_MASS, position, quaternion);
